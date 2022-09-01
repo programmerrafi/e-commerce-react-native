fe8efe2c7e5e1bda09bd29cbff07fc15230b4ac9
Md Rafi <mdrafiprogrammer@gmail.com>
2022-09-01 18:38:23 +0600

    data-set and iamges set-up done

--- app/data/products.js
+++ app/data/products.js
@@ -10,4 +10,59 @@ export const products = [
     rating: 4,
     numReviews: 4,
   },
+  {
+    _id: "2",
+    name: "Smartwatch",
+    image: "require(assets/w2.png)",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid",
+    price: 589,
+    countInStock: 5,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Smartwatch",
+    image: "require(assets/w3.png)",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid",
+    price: 839,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Smartwatch",
+    image: "require(assets/w4.png)",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid",
+    price: 400,
+    countInStock: 8,
+    rating: 4,
+    numReviews: 3,
+  },
+  {
+    _id: "5",
+    name: "Smartwatch",
+    image: "require(assets/w5.png)",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid",
+    price: 639,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: "6",
+    name: "Smartwatch",
+    image: "require(assets/w6.png)",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid",
+    price: 562,
+    countInStock: 6,
+    rating: 4,
+    numReviews: 4,
+  },
 ];
