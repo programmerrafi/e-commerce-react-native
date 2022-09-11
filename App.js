7d0276a7d6833401ebfa8f3050828e1dc70b0665
Md Rafi <mdrafiprogrammer@gmail.com>
2022-09-12 04:42:38 +0600

    navigation setup done

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
+import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 import OderScreen from "./app/screens/OderScreen";
+import { Provider } from "react-redux";
+import { store } from "./app/Redux/store";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomNav from "./app/navigations/BottomNav";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <TailwindProvider>
-        <OderScreen />
-      </TailwindProvider>
+      <NavigationContainer>
+        <Provider store={store}>
+          <TailwindProvider>
+            <Stack.Navigator
+              initialRouteName="Bottom"
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Stack.Screen name="LoginScreen" component={LoginScreen} />
+              <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+              <Stack.Screen name="OderScreen" component={OderScreen} />
+              <Stack.Screen name="Bottom" component={BottomNav} />
+            </Stack.Navigator>
+          </TailwindProvider>
+        </Provider>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ app/navigations/BottomNav.js
@@ -0,0 +1,27 @@
+import {
+  Pressable,
+  Text,
+  Box,
+  ScrollView,
+  View,
+  Flex,
+  Image,
+  Heading,
+  Center,
+  HStack,
+  Button,
+  VStack,
+} from "native-base";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { Colors } from "../configs/color";
+import React from "react";
+
+const BottomNav = () => {
+  return (
+    <Box safeAreaTop>
+      <Text>BottomNav</Text>
+    </Box>
+  );
+};
+
+export default BottomNav;
